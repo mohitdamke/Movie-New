4dc4f2022bcb3f375fd0f6e4c298dec3ec9e6b25
raees <mohitdamke1@gmail.com>
2024-07-09 10:55:38 +0530

    First

--- app/src/main/java/com/example/mymovie/main/data/remote/dto/GenresListDto.kt
+++ app/src/main/java/com/example/mymovie/main/data/remote/dto/GenresListDto.kt
@@ -1,5 +1,7 @@
 package com.example.mymovie.main.data.remote.dto
 
+import com.example.mymovie.main.domain.models.Genre
+
 data class GenresListDto(
     val genres: List<Genre>
 )
---
+++ app/src/main/java/com/example/mymovie/main/domain/models/Genre.kt
@@ -0,0 +1,6 @@
+package com.example.mymovie.main.domain.models
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
